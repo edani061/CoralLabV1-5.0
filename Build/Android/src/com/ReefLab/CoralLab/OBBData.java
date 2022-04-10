7fa7209f16e6041d4673a8cdd393f3731d9bf646
edani061 <33548334+edani061@users.noreply.github.com>
2022-04-10 18:08:49 +0800

    High and Low
    High and low resolution maps were created. Also added more post processing

--- Build/Android/src/com/ReefLab/CoralLab/OBBData.java
+++ Build/Android/src/com/ReefLab/CoralLab/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-936363656L // the length of the file in bytes
+918364732L // the length of the file in bytes
 )
 };
 };
